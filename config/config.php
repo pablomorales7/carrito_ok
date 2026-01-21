f9ef8395cdc870e3cc8e0873db857247b7363e51
pablomorales7 <151417575+pablomorales7@users.noreply.github.com>
2026-01-20 22:23:52 -0500

    commit carrito ok2

--- config/config.php
+++ config/config.php
@@ -1,8 +1,14 @@
 <?php
-$usuario = "root"; // Reemplaza con el nombre de usuario correcto
+/*$usuario = "root"; // Reemplaza con el nombre de usuario correcto
 $password = ""; // Reemplaza con la contraseña correcta
 $servidor = "localhost";
-$basededatos = "bd_tienda_online";
+$basededatos = "bd_tienda_online";*/
+
+$usuario = getenv("DB_USER"); // Reemplaza con el nombre de usuario correcto
+$password = getenv("DB_PASS"); // Reemplaza con la contraseña correcta
+$servidor = getenv("DB_HOST");
+$basededatos = getenv("DB_NAME");
+$puerto = getenv("DB_PORT") ?: 3306;
 
 // Crear la conexión
 $con = mysqli_connect($servidor, $usuario, $password, $basededatos);
